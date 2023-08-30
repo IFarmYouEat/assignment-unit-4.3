6d7743d549714f7ac7ab695d099be0336fefb36a
Brock Nelson <Bmnelson87@gmail.com>
2023-08-30 11:26:54 -0500

    base is complete, working on stretch

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,16 +1,37 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
 
-function addItem() {
-    basket.push('Kale');
+let basket = [];
+const maxItems = 5;
+
+function addItem(item) {
+    basket.push(item);
+    return true;
 }
 
+function listItems(cart){
+    for(let item of cart){
+        console.log(item);
+    }
+}
+
+addItem('chicken');
+addItem('pasta');
+addItem('milk');
 
 
+console.log(basket);
 
+console.log(listItems(basket));
 
+function isFull(cart){
+    if(cart === 5){
+        return true;
+    }else if (cart != 5){
+        return false;
+    }
+}
 
 
 
